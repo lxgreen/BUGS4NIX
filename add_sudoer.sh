236150ea4ed94bdd85a852ca816c58b858cc22cb
lxgreen <alxgreen@gmail.com>
2016-06-01 23:10:58 +0300

    add_sudoer syntax

--- add_sudoer.sh
+++ add_sudoer.sh
@@ -1,13 +1,5 @@
-if [ "$1" -ne "/etc/sudoers" ]; then
-
-  # When you run the script, you will run this block since $1 is empty.
-  # We first set this script as the EDITOR and then starts visudo.
-  # Visudo will now start and use THIS SCRIPT as its editor
-  export NEW_SUDOER=$1 && export EDITOR=$0 && sudo -E visudo
-else
-  # When visudo starts this script, it will provide the name of the sudoers 
-  # file as the first parameter and $1 will be non-empty. Because of that, 
-  # visudo will run this block.
-  # We change the sudoers file and then exit  
+if [ "$1" != "/etc/sudoers" ]; then 
+  export NEW_SUDOER=$1 && export EDITOR=$0 && -E visudo
+else  
   echo $NEW_SUDOER >> $1
 fi
